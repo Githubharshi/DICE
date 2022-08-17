41d01e7a9cffc02c0d5e574e49251f09023b4578
Githubharshi <99710451+Githubharshi@users.noreply.github.com>
2022-08-17 19:59:37 +0530

    files uploaded

---
+++ dice.js
@@ -0,0 +1,16 @@
+var random1=Math.floor(Math.random()*6)+1;
+var image1="images/dice"+random1+".png";
+document.querySelectorAll("img")[0].setAttribute("src",image1);
+
+var random2=Math.floor(Math.random()*6)+1;
+var image2="images/dice"+random2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",image2);
+
+if(random1>random2){
+  document.querySelector("h1").innerHTML="🎉Player 1 WINS🎉";
+}
+else if(random1<random2){
+  document.querySelector("h1").innerHTML=" 🎉Players 2 WINS🎉";
+}else{
+  document.querySelector("h1").innerHTML=" DRAW🙌";
+}
