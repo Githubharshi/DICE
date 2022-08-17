1c46841a922ba25948288c6c7481439968c18803
Githubharshi <99710451+Githubharshi@users.noreply.github.com>
2022-08-17 20:02:16 +0530

    updated

--- dice.js
+++ dice.js
@@ -1,9 +1,9 @@
 var random1=Math.floor(Math.random()*6)+1;
-var image1="images/dice"+random1+".png";
+var image1="dice"+random1+".png";
 document.querySelectorAll("img")[0].setAttribute("src",image1);
 
 var random2=Math.floor(Math.random()*6)+1;
-var image2="images/dice"+random2+".png";
+var image2="dice"+random2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",image2);
 
 if(random1>random2){
